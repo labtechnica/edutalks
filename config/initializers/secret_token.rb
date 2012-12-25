f8a0420c2b194f5b0eef64b92a8660d9fed17809
Stephen Baidu <labtechnica@gmail.com>
2012-12-25 12:50:17 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Edutalks::Application.config.secret_token = '129a0ca3bd74b782ac7ff0b639d2f6a34e152d22d458ac56133f0668bba84a088437c1736928b2fb9de470b6ae9606caaa91903607822d2e900f771597ab450f'
